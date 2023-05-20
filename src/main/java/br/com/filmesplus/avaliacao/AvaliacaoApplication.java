6260f24167ef1371d5056853cc6e33ddc8fb4114
Gabriel Fernandes <gaabezk@gmail.com>
2023-05-20 11:50:56 -0300

    fixx

--- src/main/java/br/com/filmesplus/avaliacao/AvaliacaoApplication.java
+++ src/main/java/br/com/filmesplus/avaliacao/AvaliacaoApplication.java
@@ -1,14 +1,24 @@
 package br.com.filmesplus.avaliacao;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
 
 @SpringBootApplication
+@OpenAPIDefinition(
+        info = @Info(
+                title = "AvaliaçãoPlus - Api",
+                version = "v1",
+                description = "SpringBoot Web Api",
+                contact = @io.swagger.v3.oas.annotations.info.Contact(
+                        name = "Gabriel Fernandes",
+                        email = "gabriel1521@outlook.com",
+                        url = "https://github.com/gaabezk"
+                )
+        )
+)
 public class AvaliacaoApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(AvaliacaoApplication.class, args);
     }
